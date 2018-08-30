194e9e19d66af635ddc16522c7b2d27c2d8f16b3
Parthjhaveri <parthjhaveri209@gmail.com>
2018-08-30 00:58:36 -0400

    setup initial navbar component & other folder structure

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="app-main-nav">
+        
+        Site Nav
+        
+      </div>
+    );
+  }
+}
+
+export default Navbar;
